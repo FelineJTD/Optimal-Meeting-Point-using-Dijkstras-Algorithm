8c3cced44580c228de142f1c9bdd8b85b486f54a
FelineJTD <16520335@std.stei.itb.ac.id>
2021-12-08 22:21:41 +0700

    add Dijkstra main

--- OptimalMeetingPoint.py
+++ OptimalMeetingPoint.py
@@ -1,13 +1,5 @@
-class Vertex:
-  def __init__(self, name):
-    self.name = name
-    self.edges = {}
-  def addEdge(self, destination, distance):
-    self.edges[destination] = distance
-  
 def readVertices(filename):
-  vertices = []
-  possible_meeting_points = []
+  vertices = {}
 
   with open(filename, "r") as f:
     data = f.read()
@@ -15,25 +7,89 @@ def readVertices(filename):
   data = data.replace(" ", "")
   data = data.split("\n")
   
-  i = 0
-  while(i < len(data) and data[i] != "end."):
+  for i in range(len(data)):
     if (data[i] != "" and data[i][0] != "#"):
       data[i] = data[i].split(":")
-      vertex = Vertex(data[i][0])
+      name = data[i][0]
+      vertices[name] = {}
       data[i][1] = data[i][1].split(";")
       for j in range (len(data[i][1])):
         data[i][1][j] = data[i][1][j].split(",")
-        vertex.addEdge(data[i][1][j][0], data[i][1][j][1])
-      vertices.append(vertex)
-    i += 1
+        vertices[name][data[i][1][j][0]] = int(data[i][1][j][1])
   
-  i += 1
-
-  while(i < len(data) and data[i] != "end."):
-    if (data[i] != "" and data[i][0] != "#"):
-      possible_meeting_points.append(data[i])
-    i += 1
-
-  return(vertices, possible_meeting_points)
+  return(vertices)
     
-readVertices("vertices.txt")
+
+def completeDijkstra(vertices):
+  cheapest_prices = {}
+  previous_stopover_city = {}
+  unvisited_cities = []
+  rev_cities = []
+  visited_cities = {}
+
+  for i in vertices.keys():
+    visited_cities[i] = False
+
+  start_city = i
+  unvisited_cities.append(start_city)
+  rev_cities.append(start_city)
+
+  # initial loop
+  while (unvisited_cities!=[]):
+    # initiation
+    print(unvisited_cities)
+    curr_city = unvisited_cities[0]
+    print("\ncurr_city =", curr_city)
+    cheapest_prices[curr_city] = {}
+    previous_stopover_city[curr_city] = {}
+    cheapest_prices[curr_city][curr_city] = 0
+    unvisited_cities.pop(0)
+    visited_cities[curr_city] = True
+    print(cheapest_prices)
+    print(previous_stopover_city)
+
+    for adj_city, price in vertices[curr_city].items(): # iterate each adj cities
+      if (not visited_cities[adj_city]):
+        unvisited_cities.append(adj_city)
+        rev_cities.append(adj_city)
+
+      for vc, visited in visited_cities.items():
+        if visited:
+          try:
+            price_through_current_city = cheapest_prices[vc][curr_city] + price
+            try:
+              if(cheapest_prices[vc][adj_city] > price_through_current_city):
+                cheapest_prices[vc][adj_city] = price_through_current_city
+                previous_stopover_city[vc][adj_city] = curr_city
+            except:
+              cheapest_prices[vc][adj_city] = price_through_current_city
+              previous_stopover_city[vc][adj_city] = curr_city
+          except:
+            pass
+    
+  # revisiting cities
+  for p in range(len(rev_cities)):
+    curr_city = rev_cities[p]
+    for q in range(p+1,len(rev_cities)):
+      vc = rev_cities[q]
+      for adj_city, price in vertices[rev_cities[p]].items(): # iterate each adj cities
+        try:
+          price_through_current_city = cheapest_prices[vc][curr_city] + price
+          try:
+            if(cheapest_prices[vc][adj_city] > price_through_current_city):
+              cheapest_prices[vc][adj_city] = price_through_current_city
+              previous_stopover_city[vc][adj_city] = curr_city
+          except:
+            cheapest_prices[vc][adj_city] = price_through_current_city
+            previous_stopover_city[vc][adj_city] = curr_city
+        except:
+          pass
+  
+  print(cheapest_prices)
+  return(cheapest_prices, previous_stopover_city)
+  
+
+
+  
+a = readVertices("Atlanta.txt")
+completeDijkstra(a)
